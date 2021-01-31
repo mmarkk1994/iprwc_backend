78043629e31ee5840769bf2720f568ed87f3f9a7
Mark <mmarkk1994@gmail.com>
2021-01-31 11:19:59 +0100

    set getters for HttpBody

--- src/main/java/src/core/HttpBody.java
+++ src/main/java/src/core/HttpBody.java
@@ -16,14 +16,30 @@ public class HttpBody {
         this.status = status;
     }
 
+    public Response.Status getStatus() {
+        return status;
+    }
+
     public void setMessage(String message) { this.message = message; }
+
+    public String getMessage() {
+        return message;
+    }
 
     public void setContent(Object content) {
         this.content = content;
     }
 
+    public Object getContent() {
+        return content;
+    }
+
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public String getToken() {
+        return token;
     }
 
     public Response build(){
